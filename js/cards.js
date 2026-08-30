3266a32a23d5f9b0b4d4a2f05c8fcfc42f67f5a1
yuan <ming5580@163.com>
2026-08-30 19:56:26 +0800

    .

--- js/cards.js
+++ js/cards.js
@@ -4,7 +4,7 @@ export function initCardsAnim() {
   animate(".card", {
     opacity: [0, 1],
     translateY: [24, 0],
-    delay: anime.stagger(120),
+    delay: stagger(120),
     duration: 700,
     ease: "outBack",
   });
