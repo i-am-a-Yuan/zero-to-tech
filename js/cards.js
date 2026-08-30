b240d1bec8107ca94bf844283e233f7cbb1811ec
yuan <ming5580@163.com>
2026-08-30 19:35:38 +0800

    修复cards.js

--- js/cards.js
+++ js/cards.js
@@ -1,13 +1,11 @@
 import { animate, stagger } from "https://cdn.jsdelivr.net/npm/animejs@4.0.0/+esm";
 
 export function initCardsAnim() {
-  function () {
-    anime.animate(".card", {
-      opacity: [0, 1],
-      translateY: [24, 0],
-      delay: anime.stagger(120),
-      duration: 700,
-      ease: "outBack",
-    });
-  }
+  anime.animate(".card", {
+    opacity: [0, 1],
+    translateY: [24, 0],
+    delay: anime.stagger(120),
+    duration: 700,
+    ease: "outBack",
+  });
 }
